737bb7f03b12c041f32615f08989dca1612f29b7
Just van den Broecke <just@justobjects.nl>
2014-11-07 12:37:45 +0100

    issue #2 - eerste versie IMWE2014 proefbestand: WENota+WENiveau

--- samples/imwe2008/get-imwe-wfs.sh
+++ samples/imwe2008/get-imwe-wfs.sh
@@ -5,6 +5,7 @@
 #
 /bin/rm -f *.xml *.json
 
+# GeoServer WFS
 wget -O wfs-capabilities.xml 'http://108.171.173.8/data/imwe/ows?service=WFS&version=1.1.0&request=GetCapabilities'
 wget -O wfs-featuretypes.xml 'http://108.171.173.8/data/imwe/ows?service=WFS&version=1.1.0&request=DescribeFeatureType'
 
@@ -19,3 +20,11 @@ wget -O wfs-welstandsniveaugebied1.json 'http://108.171.173.8/data/imwe/ows?serv
 
 wget -O wfs-welstandsnota1.xml 'http://108.171.173.8/data/imwe/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=imwe:welstandsnota&maxFeatures=5&outputFormat=GML3'
 wget -O wfs-welstandsnota1.json 'http://108.171.173.8/data/imwe/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=imwe:welstandsnota&maxFeatures=5&outputFormat=JSON'
+
+# imwe.nl deegree WFS
+#
+wget -O imwe.nl-welstandsnota-compleet1.xml 'http://www.imwe.nl/getAllData?longitude=100475.830078125&latitude=409607.91015625'
+wget -O imwe.nl-capabilities.xml 'http://www.imwe.nl/data?service=WFS&version=1.1.0&request=GetCapabilities'
+wget -O imwe.nl-featuretypes.xml 'http://www.imwe.nl/data?service=WFS&version=1.1.0&request=DescribeFeatureType'
+wget -O imwe.nl-welstandsniveaugebied1.xml 'http://www.imwe.nl/data?service=WFS&version=1.1.0&request=GetFeature&typeName=WelstandsNiveauGebied&maxFeatures=10'
+wget -O imwe.nl-welstandsnnota1.xml 'http://www.imwe.nl/data?service=WFS&version=1.1.0&request=GetFeature&typeName=WelstandsNota&maxFeatures=10'
