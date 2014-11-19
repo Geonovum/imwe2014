e1a4caba23e0d20eeaf775a0f6bc256af306139a
Just van den Broecke <just@justobjects.nl>
2014-11-19 14:30:51 +0100

    issue #2 - meer content in IMWE(PT)2014 proefbestanden

---
+++ samples/imwe2014/gegenereerd/README.md
@@ -0,0 +1,3 @@
+Deze map bevat bestanden direct vanuit de (stubbed) XSD schema's (zie schemas/imwe2014-stub) gegenereerd
+binnen de IDE tool IntelliJ IDEA. Deze zijn als basis gebruikt om de uiteindelijke proefbestanden op te bouwen.
+
--- samples/imwe2014/validate.sh
+++ samples/imwe2014/validate.sh
@@ -1,3 +1,5 @@
-# ../../validator/bin/validate.sh FC-IMWE2014-1.xml
-../../validator/bin/validate.sh FC-IMWEPT2014-1.xml
+# valideer: alleen voor XSD schema-compliance (syntax) NIET op inouhd (semantisch)
+../../validator/bin/validate.sh FC-IMWE2014-1.xml FC-IMWEPT2014-1.xml
+
+# ../../validator/bin/validate.sh FC-IMWEPT2014-1.xml
 
